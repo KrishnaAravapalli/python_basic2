003f79769cc2fc49b18b8544dc75e12c21540d5d
KrishnaAravapalli <krishnaravapalli334@gail.com>
2025-03-07 20:34:34 +0530

    Initial commit

---
+++ math.py
@@ -0,0 +1,15 @@
+import math
+
+
+num = float(input("Enter a number: "))
+
+
+sqrt_value = math.sqrt(num)
+log_value = math.log(num)
+sine_value = math.sin(num)
+
+
+print(f"\nResults for the number {num}:")
+print(f"Square root: {sqrt_value}")
+print(f"Logarithm: {log_value}")
+print(f"Sine: {sine_value}")
